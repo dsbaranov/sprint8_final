6fb6ebe033871f03b6b99ac72f339cad5f2d9016
baranov_d <baranov_d@stilsoft.ru>
2024-11-13 18:04:16 +0300

    started output

--- stat_reader.cpp
+++ stat_reader.cpp
@@ -1,6 +1,25 @@
 #include "stat_reader.h"
 
-void ParseAndPrintStat(const TransportCatalogue& tansport_catalogue, std::string_view request,
-                       std::ostream& output) {
-    // Реализуйте самостоятельно
+#include <sstream>
+#include <unordered_set>
+// Bus 256
+using namespace std::string_literals;
+
+void ParseAndPrintStat(const TransportCatalogue& transport_catalogue,
+                       std::string_view request, std::ostream& output) {
+  std::string_view bus_name = request.substr(
+      request.find_first_of(' ') + 1,
+      request.find_last_not_of(' ') - request.find_first_of(' '));
+
+  Bus* target = transport_catalogue.FindBus(bus_name);
+  output << "Bus "s << bus_name << ": "s;
+  if (target == nullptr) {
+    output << "not found\n";
+    return;
+  }
+  std::unordered_set<Stop*> unique_stops(target->stops.begin(),
+                                         target->stops.end());
+  output << target->stops.size() << " stops on route, " << unique_stops.size()
+         << " unique stops, "
+         << "\n"s;
 }
--- transport_catalogue.h
+++ transport_catalogue.h
@@ -80,37 +80,13 @@ struct DistancesHasher {
 
 class TransportCatalogue {
  public:
-  void AddStop(Stop &&stop) {
-    stops_.push_back(std::move(stop));
-    Stop *target = &stops_.back();
-    stopname_to_stop_[target->name] = target;
-    for (auto &second_stop : stopname_to_stop_) {
-      double dst =
-          ComputeDistance(target->coordinates, second_stop.second->coordinates);
-      distances_[std::make_pair(target, second_stop.second)] = dst;
-      distances_[std::make_pair(second_stop.second, target)] = dst;
-    }
-  }
+  void AddStop(Stop &&stop);
 
-  Stop *FindStop(std::string_view name) const {
-    if (stopname_to_stop_.find(name) == stopname_to_stop_.end()) {
-      throw std::out_of_range("stop doesn't exist");
-    }
-    return stopname_to_stop_.at(name);
-  }
+  Stop *FindStop(std::string_view name) const;
 
-  void AddBus(Bus &&bus) {
-    busses_.push_back(std::move(bus));
-    Bus *target = &busses_.back();
-    busname_to_bus_[target->name] = target;
-  }
+  void AddBus(Bus &&bus);
 
-  Bus *FindBus(std::string_view name) {
-    if (busname_to_bus_.find(name) == busname_to_bus_.end()) {
-      return nullptr;
-    }
-    return busname_to_bus_.at(name);
-  }
+  Bus *FindBus(std::string_view name) const;
 
  private:
   std::deque<Stop> stops_;
